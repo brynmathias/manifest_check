d36a3622c39c7b464123d060607da646824a6ed9
brynmathias <brynmathias@gmail.com>
2014-01-29 17:22:03 +0000

    updated the code so that we can do 200 threads

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include "iostream"
+#include <iostream>
+#include <fstream>
 #include "curl/curl.h"
 #include "rapidxml-1.13/rapidxml.hpp"
 #include "rapidxml-1.13/rapidxml_print.hpp"
 #include <thread>
 #include <vector>
+#include <sstream>
 #include <unistd.h>
 #include <chrono>
+#include <stdio.h>
+#include <string.h>
 using namespace rapidxml;
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -15,37 +19,14 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 
 
 
-std::string get_manifest_file( std::string manifest_url, CURL *curl )
+
+long long get_time_stamp( std::string manifest )
 {
-    std::string buffer;
-
-    CURLcode res;
-        curl = curl_easy_init();
-        curl_easy_setopt( curl, CURLOPT_URL, manifest_url.c_str() );
-        curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, WriteCallback );
-        curl_easy_setopt( curl, CURLOPT_WRITEDATA, &buffer );
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-
-    if(res == CURLE_OK)
-        return buffer;
-    else{
-        std::cout << "failed curl";
-        exit(0);
-    }
-}
-
-
-long long get_time_stamp( std::string url, CURL *curl )
-{
-    std::string manifest = get_manifest_file(url, curl);    
-    xml_document<> doc;    
+    xml_document<> doc;
     xml_node<> *root_node;
     char * cstr = new char[manifest.size() +1];
-    // std::cout << manifest << std::endl;
     strcpy(cstr, manifest.c_str());
     doc.parse<0> ( cstr );
-    // print(std::cout, doc, 0);
     root_node = doc.first_node("SmoothStreamingMedia");
     xml_node<> * video_node = root_node->first_node("StreamIndex");
     if(strcmp(video_node->first_attribute("Type")->value(), "video") != 0 && video_node->next_sibling() != NULL )
@@ -55,28 +36,69 @@ long long get_time_stamp( std::string url, CURL *curl )
 }
 
 
-void run_manifest_check(std::string url)
+void run_manifest_check(int channel)
 {
+
+    char url[100];
+    sprintf(url,"http://hout.livec3.c9.id.cam7.eng.velocix.com/smj%d/smooth/Manifest",
+            channel);
+    std::string buffer;
+    CURL *curl;
+    int count = 0;
+    CURLcode res;
+    curl = curl_easy_init();
+    curl_easy_setopt( curl, CURLOPT_URL, url );
+    curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, WriteCallback );
+    curl_easy_setopt( curl, CURLOPT_WRITEDATA, &buffer );
+    std::stringstream localBuffer;
     int max_time_difference = 20020000;
     long long current_segment_time = 0;
     long long previous_segment_time = 0;
-    CURL *curl;
+    int max_time = 0;
+    bool clear_counter = false;
+    int update_counter = 0;
     for(size_t i = 0; i < 500; ++i)
     {
-        previous_segment_time = current_segment_time;
-        current_segment_time = get_time_stamp(url,curl);
+        res = curl_easy_perform(curl);
+        if(current_segment_time != previous_segment_time){
+            clear_counter = true;
+            previous_segment_time = current_segment_time;
+        }
+        else
+        {
+            update_counter += 1;
+        }
+        current_segment_time = get_time_stamp(buffer);
         if(current_segment_time - previous_segment_time > max_time_difference)
         {
-            std::cout << "time difference is larger than expected at: " << current_segment_time - previous_segment_time << "\n URL: " << url <<std::endl;
+            count++;
+            if(count > 1){
+                localBuffer << "time difference is larger than expected at: " << current_segment_time - previous_segment_time 
+                    << " count is "<< count << "\n URL: " << url << "\n took " << update_counter << " manifest refreshes to update." << std::endl;
+                max_time += current_segment_time - previous_segment_time;
+            }
         }
-        std::chrono::milliseconds dura( 1500 );
+        if(clear_counter == true)
+        {
+            update_counter = 0;
+            clear_counter = false;
+
+        }
+        std::chrono::milliseconds dura( 500 );
         std::this_thread::sleep_for( dura );
+        buffer.clear();
     }
 
-    
-    
-    
-    
+    char fname[50];
+    localBuffer << "!!!!!!!!!\n average time over 2 seconds is " << (max_time)/(count-1) << " for n = " << count << std::endl;
+    sprintf(fname,"./stream_%d.log", channel);
+    std::ofstream file;
+    file.open(fname, std::ios::out);
+    file << localBuffer.str();
+
+    file.close();
+    curl_easy_cleanup(curl);
+
 }
 
 
@@ -84,15 +106,14 @@ void run_manifest_check(std::string url)
 
 int main (int argc, char const *argv[])
 {
-    int num_threads = 8;
-    std::vector<std::string> urls = {"http://hout.livec3.c9.id.cam7.eng.velocix.com/smj1/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj2/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj3/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj4/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj5/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj6/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj7/smooth/Manifest","http://hout.livec3.c9.id.cam7.eng.velocix.com/smj8/smooth/Manifest"};
-    std::vector<std::thread> t;    
+    int num_threads = 199;
+    std::vector<std::thread> t;
     // std::cout << "video time stamp = " << get_time_stamp( "http://hout.livec3.c9.id.cam7.eng.velocix.com/smj1/smooth/Manifest") << std::endl;
     for (int i = 0; i < num_threads; ++i) {
-         t.push_back(std::thread(run_manifest_check, urls[i]));
+         t.push_back(std::thread(run_manifest_check, i+1));
      }
-     
-     for (auto &th: t) {
+
+     for (auto &th : t) {
          th.join();
      }
     // print(std::cout, doc, 0);   // 0 means default printing flags
